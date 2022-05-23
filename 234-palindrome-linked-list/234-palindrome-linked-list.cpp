21601ba4911cb97c5ca4ae83005b1eb456713278
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-05-23 23:51:23 +0530

    Time: 341 ms (33.17%), Space: 118 MB (59.10%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,18 +10,52 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        vector<int> temp;
-        ListNode *curr=head;
-        while(curr){
-            temp.push_back(curr->val);
-            curr=curr->next;
+    //brute force
+    // bool isPalindrome(ListNode* head) {
+    //     vector<int> temp;
+    //     ListNode *curr=head;
+    //     while(curr){
+    //         temp.push_back(curr->val);
+    //         curr=curr->next;
+    //     }
+    //     int l=0,h=temp.size()-1;
+    //     while(l<h){
+    //         if(temp[l++]!=temp[h--])
+    //             return false;
+    //     }
+    //     return true;
+    // }
+    
+    
+    //optimized
+    ListNode* reverse(ListNode* ptr) {
+        ListNode *pre=NULL;
+        ListNode* nxt=NULL;
+        while(ptr!=NULL) {
+            nxt = ptr->next;
+            ptr->next = pre;
+            pre=ptr;
+            ptr=nxt;
         }
-        int l=0,h=temp.size()-1;
-        while(l<h){
-            if(temp[l++]!=temp[h--])
+        return pre;
+    }
+
+    bool isPalindrome(ListNode* head){
+        if(!head || !head->next) return true;
+        ListNode *slow=head,*fast = head;        
+        while(fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }        
+        slow->next = reverse(slow->next);
+        slow=slow->next;
+        fast=head;
+        while(slow!=NULL) {
+            if(fast->val != slow->val) 
                 return false;
+            fast = fast->next;
+            slow = slow->next;
         }
         return true;
-    }
+    }    
 };
