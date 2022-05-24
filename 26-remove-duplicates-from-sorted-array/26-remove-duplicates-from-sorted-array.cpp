6ecc463fc96f43572151ceec94113f8999c04488
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-05-24 08:59:37 +0530

    Time: 19 ms (42.30%), Space: 18.2 MB (92.83%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
-    //optimal
     int removeDuplicates(vector<int>& nums) {
         int l=0,r=0;
         while(r<nums.size()){
-            if(nums[l]!=nums[r])  nums[++l]=nums[r];
-            r++; 
+            if(nums[l]==nums[r])
+                r++;
+            else
+                nums[++l]=nums[r++];
         }        
         return l+1;
     }
