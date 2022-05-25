457dd09aee65234b2990eefabd113bb1521412f0
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-05-25 13:03:12 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (78.45%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,51 @@
+class Solution {
+    /*private:
+    void recur(vector<int>&ds, vector<vector<int>>&ans, vector<int>&freq,
+               vector<int>&nums)
+    {
+        if(ds.size()==nums.size())
+        {
+            ans.push_back(ds);
+            return;
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(!freq[i])
+            {
+                freq[i]=1;
+                ds.push_back(nums[i]);
+                recur(ds,ans,freq,nums);
+                freq[i]=0;
+                ds.pop_back();
+            }
+        }
+    }
+    public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int>ds;
+        vector<int> freq(nums.size(),0);
+        recur(ds,ans,freq,nums);        
+        return ans;
+    }*/
+    //private:
+    
+    
+    public:
+    void recur(int ind, vector<vector<int>>&ans, vector<int>&nums)    {
+        if(ind==nums.size())        {
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=ind;i<nums.size();i++)        {
+            swap(nums[ind],nums[i]);
+            recur(ind+1,ans,nums);
+            swap(nums[i],nums[ind]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        recur(0,ans,nums);
+        return ans;
+    }    
+};
