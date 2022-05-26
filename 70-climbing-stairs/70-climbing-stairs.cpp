7a54de4a7ebf3c74325255f09b487dacb02681d6
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-05-26 11:48:59 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.53%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -25,12 +25,12 @@ public:
     
     //space optimization 
     int climbStairs(int n){
-        int prev=1,prev2=1,curr=1;
+        int prev=1,prev2=1;
         for(int i=2;i<=n;i++){   
-            curr=prev+prev2;
+            int curr=prev+prev2;
             prev2=prev;
             prev=curr;
         }
-        return curr;
+        return prev;
     }
 };
