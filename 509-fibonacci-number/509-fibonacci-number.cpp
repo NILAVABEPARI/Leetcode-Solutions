e2ee4b546c2a59786ae8075bf8dd2b055518a159
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-05-26 11:25:58 +0530

    Time: 4 ms (51.09%), Space: 5.9 MB (42.34%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -12,12 +12,24 @@ public:
     // }
     
     //tabulation
+    // int fib(int n){
+    //     vector<int>dp(n+1,0);
+    //     dp[0]=0;
+    //     if(n>0) dp[1]=1;
+    //     for(int i=2;i<=n;i++)
+    //         dp[i]=dp[i-1]+dp[i-2];
+    //     return dp[n];
+    // }    
+    
+    //space optimization
     int fib(int n){
-        vector<int>dp(n+1,0);
-        dp[0]=0;
-        if(n>0) dp[1]=1;
-        for(int i=2;i<=n;i++)
-            dp[i]=dp[i-1]+dp[i-2];
-        return dp[n];
-    }    
+        if(n<=1) return n;
+        int prev2=0,prev1=1;
+        for(int i=2;i<=n;i++){
+            int curr=prev2+prev1;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
 };
