a47f127ef394e4e7832993f5e28adba0c573f68f
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-05-26 21:33:38 +0530

    Time: 5 ms (19.52%), Space: 7.7 MB (76.43%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -16,16 +16,32 @@ public:
     // }
     
     //bottom up - tabulation
+    // int rob(vector<int>& nums){
+    //     int n=nums.size();
+    //     vector<int>dp(n,0);
+    //     dp[0]=nums[0];
+    //     for(int i=1;i<n;i++){
+    //         int pick=nums[i];
+    //         if(i>1) pick+=dp[i-2];
+    //         int n_pick=dp[i-1];
+    //         dp[i]=max(pick,n_pick);
+    //     }
+    //     return dp[n-1];
+    // }
+    
+    
+    //space optimization
     int rob(vector<int>& nums){
         int n=nums.size();
-        vector<int>dp(n,0);
-        dp[0]=nums[0];
+        int prev2=0,prev1=nums[0];
         for(int i=1;i<n;i++){
             int pick=nums[i];
-            if(i>1) pick+=dp[i-2];
-            int n_pick=dp[i-1];
-            dp[i]=max(pick,n_pick);
+            if(i>1) pick+=prev2;
+            int n_pick=prev1;
+            int curr=max(pick,n_pick);
+            prev2=prev1;
+            prev1=curr;
         }
-        return dp[n-1];
+        return prev1;
     }
 };
