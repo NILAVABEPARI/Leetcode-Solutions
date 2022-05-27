0c2a4f62533106db76d78ca06a563419257f6f5f
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-05-27 20:50:20 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -5,29 +5,62 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
   public:
-  bool f(int i, int sum, int arr[],vector<vector<int>>&dp){
-        if(sum==0) return dp[i][sum]=true;
-        if(i==0) return dp[i][sum] = (arr[0]==sum);
-        if(dp[i][sum]!=-1) return dp[i][sum];
+  //memoization
+//   bool f(int i, int sum, int arr[],vector<vector<int>>&dp){
+//         if(sum==0) return dp[i][sum]=true;
+//         if(i==0) return dp[i][sum] = (arr[0]==sum);
+//         if(dp[i][sum]!=-1) return dp[i][sum];
         
-        bool ntake=f(i-1,sum,arr,dp);
-        bool take=false;
-        if(arr[i]<=sum)
-            take=f(i-1,sum-arr[i],arr,dp);
-        return dp[i][sum]=take or ntake;
-    }
-    int minDifference(int arr[], int n) {
-        int sum = 0;
-        for (int i = 0; i < n; i++) sum += arr[i];
-        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+//         bool ntake=f(i-1,sum,arr,dp);
+//         bool take=false;
+//         if(arr[i]<=sum)
+//             take=f(i-1,sum-arr[i],arr,dp);
+//         return dp[i][sum]=take or ntake;
+//     }
+//     int minDifference(int arr[], int n) {
+//         int sum = 0;
+//         for (int i = 0; i < n; i++) sum += arr[i];
+//         vector<vector<int>>dp(n,vector<int>(sum+1,-1));
         
-        for (int i = 0; i <= sum; i++) 
-            bool flag=f(n-1,i,arr,dp);
+//         for (int i = 0; i <= sum; i++) 
+//             bool flag=f(n-1,i,arr,dp);
         
+//         int mini = 1e9;
+//         for (int i = 0; i <= sum/2; i++) {
+//             if (dp[n - 1][i] == true) {
+//                 int diff = abs(i - (sum - i));
+//                 mini = min(mini, diff);
+//             }
+//         }
+//         return mini;
+//     }
+
+
+    //tabulation
+    int minDifference(int arr[], int n)  { 
+        int totSum = 0;
+        for (int i = 0; i < n; i++)
+            totSum += arr[i];
+        vector < vector < bool >> dp(n, vector < bool > (totSum + 1, false));
+        for (int i = 0; i < n; i++) 
+            dp[i][0] = true;
+            
+        if (arr[0] <= totSum)
+            dp[0][totSum] = true;
+
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 1; target <= totSum; target++) {
+                bool notTaken = dp[ind - 1][target];
+                bool taken = false;
+                if (arr[ind] <= target)
+                    taken = dp[ind - 1][target - arr[ind]];
+                dp[ind][target] = notTaken || taken;
+            }
+        } 
         int mini = 1e9;
-        for (int i = 0; i <= sum/2; i++) {
+        for (int i = 0; i <= totSum; i++) {
             if (dp[n - 1][i] == true) {
-                int diff = abs(i - (sum - i));
+                int diff = abs(i - (totSum - i));
                 mini = min(mini, diff);
             }
         }
@@ -35,7 +68,8 @@ class Solution{
     }
     
     
-// 	int minDifference(int arr[], int n)  { 
+    //space optimizaiton
+//     int minDifference(int arr[], int n)  { 
 // 	    int sum=0;
 //         for(int i=0;i<n;i++) sum+=arr[i];
 //         vector<bool>prev(sum+1,0),curr(sum+1,0);
