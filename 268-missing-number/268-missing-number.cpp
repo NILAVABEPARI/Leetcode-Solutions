8b372ebfb13cc4b1b2d733d72b5991690bd0c988
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-05-28 21:31:24 +0530

    Time: 28 ms (46.47%), Space: 18 MB (20.98%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
+    //bture force
+    // int missingNumber(vector<int>& nums) {
+    //     int n=nums.size();
+    //     vector<int>freq(n+1,0);
+    //     for(int i=0;i<n;i++)
+    //         freq[nums[i]]=1;
+    //     for(int i=0;i<=n;i++){
+    //         if(freq[i]==0) return i;
+    //     }            
+    //     return 1;
+    // }
+    
+    //optimal
     int missingNumber(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>freq(n+1,0);
-        for(int i=0;i<n;i++)
-            freq[nums[i]]=1;
-        for(int i=0;i<=n;i++){
-            if(freq[i]==0) return i;
-        }            
-        return 1;
+        int result=nums.size();
+        for(int i=0;i<nums.size();i++){
+            result^=nums[i];
+            result^=i;
+        }
+        return result;
     }
 };
