c97b8aec485d2540fa09c34d8a9f0171c5723a6a
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-05-29 22:30:51 +0530

    Time: 27 ms (16.40%), Space: 8.5 MB (65.01%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -0,0 +1,28 @@
+struct myComp {
+    bool operator()(pair<char, int> const& a,pair<char, int> const& b){
+        return a.second < b.second;
+    }
+};
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int>mp;
+        for(auto it:s)
+            mp[it]++;
+        string ans="";
+        priority_queue<pair<char,int>,vector<pair<char,int>>,myComp>pq;
+        for(auto it:mp){
+            pq.push({it.first,it.second});
+        }
+        while(!pq.empty()){
+            int x=pq.top().second;
+            char c=pq.top().first;
+            while(x){
+                ans+=c;
+                x--;
+            }
+            pq.pop();
+        }
+        return ans;
+    }
+};
