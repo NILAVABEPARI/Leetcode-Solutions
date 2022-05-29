f14f2a5897b87404b5f0cfd5d78923bcfb804f4a
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-05-29 22:35:39 +0530

    Time: 7 ms (97.59%), Space: 8.4 MB (65.01%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -1,28 +1,52 @@
-struct myComp {
-    bool operator()(pair<char, int> const& a,pair<char, int> const& b){
-        return a.second < b.second;
-    }
-};
+// struct myComp {
+//     bool operator()(pair<char, int> const& a,pair<char, int> const& b){
+//         return a.second < b.second;
+//     }
+// };
+// class Solution {
+// public:
+//     string frequencySort(string s) {
+//         unordered_map<char,int>mp;
+//         for(auto it:s)
+//             mp[it]++;
+//         string ans="";
+//         priority_queue<pair<char,int>,vector<pair<char,int>>,myComp>pq;
+//         for(auto it:mp){
+//             pq.push({it.first,it.second});
+//         }
+//         while(!pq.empty()){
+//             int x=pq.top().second;
+//             char c=pq.top().first;
+//             while(x){
+//                 ans+=c;
+//                 x--;
+//             }
+//             pq.pop();
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
+//without comparator
 class Solution {
 public:
-    string frequencySort(string s) {
+    string frequencySort(string s){
         unordered_map<char,int>mp;
         for(auto it:s)
             mp[it]++;
         string ans="";
-        priority_queue<pair<char,int>,vector<pair<char,int>>,myComp>pq;
-        for(auto it:mp){
-            pq.push({it.first,it.second});
-        }
+        priority_queue<pair<int,char>>pq;
+        for(auto it:mp)
+            pq.push({it.second,it.first});
         while(!pq.empty()){
-            int x=pq.top().second;
-            char c=pq.top().first;
-            while(x){
-                ans+=c;
-                x--;
-            }
+            int x=pq.top().first;
+            while(x--)
+                ans+=pq.top().second;
             pq.pop();
         }
-        return ans;
+        return ans;        
     }
 };
