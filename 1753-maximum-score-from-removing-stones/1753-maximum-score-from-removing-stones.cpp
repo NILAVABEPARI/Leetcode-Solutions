b04e5b64d9890ac3c3950d59c36bb549cb7bd8b6
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-05-30 01:41:40 +0530

    Time: 151 ms (25.63%), Space: 5.9 MB (71.17%) - LeetHub

---
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumScore(int a, int b, int c) {
+        int cnt=0;
+        priority_queue<int>pq;
+        pq.push(a);
+        pq.push(b);
+        pq.push(c);
+        while(1){
+            int x=pq.top();
+            pq.pop();
+            int y=pq.top();
+            pq.pop();
+            if(y==0) return cnt;
+            x--;y--;
+            pq.push(x);
+            pq.push(y);
+            cnt++;
+        }
+        return cnt;
+    }
+};
