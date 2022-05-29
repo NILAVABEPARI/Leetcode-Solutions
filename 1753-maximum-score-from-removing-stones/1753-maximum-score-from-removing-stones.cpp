e57cb35f31092420bf3f6f639d77f6a1671d262e
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-05-30 01:48:45 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (93.87%) - LeetHub

--- 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     int maximumScore(int a, int b, int c) {
-        int cnt=0;
-        priority_queue<int>pq;
-        pq.push(a); pq.push(b); pq.push(c);
-        while(1){
-            int x=pq.top(); pq.pop();
-            int y=pq.top(); pq.pop();
-            if(y==0) return cnt;
-            x--;y--;
-            pq.push(x); pq.push(y);
-            cnt++;
-        }
-        return cnt;
+        // int cnt=0;
+        // priority_queue<int>pq;
+        // pq.push(a); pq.push(b); pq.push(c);
+        // while(1){
+        //     int x=pq.top(); pq.pop();
+        //     int y=pq.top(); pq.pop();
+        //     if(y==0) return cnt;
+        //     x--;y--;
+        //     pq.push(x); pq.push(y);
+        //     cnt++;
+        // }
+        // return cnt;
+        
+        //shorter 
+        int m=max(a,max(b,c));
+        //if 1 number is greater than sum of other 2 then answer is sum of other 2
+        if(2*m > a+b+c) return (a+b+c-m);
+        else return (a+b+c)/2;
     }
 };
