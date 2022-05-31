799223155ee4ef938a08c13b82a0d4108f0ccb15
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-05-31 21:24:29 +0530

    Time: 34 ms (80.42%), Space: 6.5 MB (82.83%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -2,19 +2,20 @@ int mod = (int)1e9+7;
 class Solution {
 public:
     //memoization
-    int f(int i, int j, string &s, string &t,vector<vector<int>>&dp){
-        if(j<0) return 1;
-        if(i<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j])
-            return dp[i][j]=(f(i-1,j-1,s,t,dp)+f(i-1,j,s,t,dp))%mod; 
-        else return dp[i][j]=f(i-1,j,s,t,dp);
-    }
-    int numDistinct(string s, string t) {
-        int n=s.length(),m=t.length();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,s,t,dp);
-    }
+    // &s and &t
+    // int f(int i, int j, string &s, string &t,vector<vector<int>>&dp){
+    //     if(j<0) return 1;
+    //     if(i<0) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(s[i]==t[j])
+    //         return dp[i][j]=(f(i-1,j-1,s,t,dp)+f(i-1,j,s,t,dp))%mod; 
+    //     else return dp[i][j]=f(i-1,j,s,t,dp);
+    // }
+    // int numDistinct(string s, string t) {
+    //     int n=s.length(),m=t.length();
+    //     vector<vector<int>>dp(n,vector<int>(m,-1));
+    //     return f(n-1,m-1,s,t,dp);
+    // }
     
     //tabulation
     // int numDistinct(string s, string t) {
@@ -32,19 +33,19 @@ public:
     // }
     
     //space optimization
-    // int numDistinct(string s, string t) {
-    //     int n=s.length(),m=t.length();
-    //     vector<int>prev(m+1,0);
-    //     for(int i=0;i<=n;i++) prev[0]=1;      
-    //     for(int i=1;i<=n;i++){
-    //         vector<int>curr(m+1,0);
-    //         for(int j=1;j<=m;j++){
-    //             if(s[i-1]==t[j-1])
-    //                 curr[j]=(prev[j-1]+prev[j])%mod;
-    //         else curr[j]=prev[j];
-    //         }
-    //         prev=curr;
-    //     }
-    //     return prev[m];
-    // }
+    int numDistinct(string s, string t) {
+        int n=s.length(),m=t.length();
+        vector<double>prev(m+1,0),curr(m+1,0);
+        //important base case
+        prev[0]=curr[0]=1;      
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1])
+                    curr[j]=(prev[j-1]+prev[j]);
+            else curr[j]=prev[j];
+            }
+            prev=curr;
+        }
+        return (int)prev[m];
+    }
 };
