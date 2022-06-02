b73eb3c6c5e3d6fa4f05f9860a15054f0094d746
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-06-02 09:08:46 +0530

    Time: 57 ms (78.34%), Space: 8.9 MB (49.28%) - LeetHub

---
+++ 368-largest-divisible-subset/368-largest-divisible-subset.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void LIS(vector<int>& nums, vector<int>&temp){
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<int>dp(n,1),hash(n);
+        int maxi=1,lastInd=0;
+        for(int i=0;i<n;i++){
+            hash[i]=i;
+            for(int prev=0;prev<i;prev++){
+                if(nums[i]%nums[prev]==0 && 1+dp[prev]>dp[i]){
+                    dp[i]=1+dp[prev];
+                    hash[i]=prev;
+                }
+            }
+            if(dp[i]>maxi){
+                maxi=dp[i];
+                lastInd=i;
+            }
+        }
+        temp.push_back(nums[lastInd]);
+        while(hash[lastInd]!=lastInd){
+            lastInd=hash[lastInd];
+            temp.push_back(nums[lastInd]);        
+        }
+        reverse(temp.begin(),temp.end());
+        // for(auto &it:temp) cout<<it<<" ";
+        // cout<<endl;
+        // return maxi;
+    }
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        vector<int>temp;
+        LIS(nums,temp);
+        return temp;
+    }
+};
