afd0ae381a3b58975bde0373df11bdeca4326fb1
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-06-20 20:39:31 +0530

    Time: 3 ms (58.45%), Space: 8.4 MB (40.89%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -12,18 +12,18 @@
 class Solution {
 public:
     //recursive
-    void it(TreeNode *root, vector<int>&ans){
-        if(root){
-            it(root->left,ans);
-            ans.push_back(root->val);
-            it(root->right,ans);
-        }
-    }
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        it(root,ans);
-        return ans;
-    }
+    // void it(TreeNode *root, vector<int>&ans){
+    //     if(root){
+    //         it(root->left,ans);
+    //         ans.push_back(root->val);
+    //         it(root->right,ans);
+    //     }
+    // }
+    // vector<int> inorderTraversal(TreeNode* root) {
+    //     vector<int>ans;
+    //     it(root,ans);
+    //     return ans;
+    // }
     
     //iterative
     // vector<int> inorderTraversal(TreeNode* root) {
@@ -31,8 +31,7 @@ public:
     //     TreeNode *node = root;
     //     stack<TreeNode*>st;
     //     while(node!=NULL || st.size()!=0){
-    //         while(node!=NULL)
-    //         {
+    //         while(node!=NULL){
     //             st.push(node);
     //             node=node->left;
     //         }
@@ -43,4 +42,30 @@ public:
     //     }
     //     return v;
     // }
+    
+    //morris traversal
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>v;
+        TreeNode *curr=root;
+        while(curr){
+            if(!curr->left){
+                v.push_back(curr->val);
+                curr=curr->right;
+            }else{                
+                TreeNode *prev=curr->left;
+                while(prev->right!=NULL && prev->right!=curr)
+                    prev=prev->right;               
+                if(prev->right==NULL){
+                    prev->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=NULL;
+                    v.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }
+        return v;
+    }    
 };
