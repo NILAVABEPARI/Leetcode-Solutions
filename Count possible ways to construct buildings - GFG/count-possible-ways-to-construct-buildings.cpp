5facebd80e13cbb88c0cf86c87eccbce015f3b3d
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-06-23 22:56:31 +0530

    Added solution - LeetHub

--- Count possible ways to construct buildings - GFG/count-possible-ways-to-construct-buildings.cpp
+++ Count possible ways to construct buildings - GFG/count-possible-ways-to-construct-buildings.cpp
@@ -6,17 +6,28 @@ using namespace std;
 class Solution{
 	public:
 	//memoization
-	int f(int N, vector<long long int>&dp){
-	    if(N==1) return 2;
-	    if(N==2) return 3;
-	    if(dp[N]!=-1) return dp[N];
-	    return dp[N]=(f(N-1,dp)+f(N-2,dp))%1000000007;
-	}
+// 	int f(int N, vector<long long int>&dp){
+// 	    if(N==1) return 2;
+// 	    if(N==2) return 3;
+// 	    if(dp[N]!=-1) return dp[N];
+// 	    return dp[N]=(f(N-1,dp)+f(N-2,dp))%1000000007;
+// 	}
+// 	int TotalWays(int N){
+// 	    // Code here
+// 	    vector<long long int>dp(N+1,-1);
+// 	    long long int temp=f(N,dp);
+// 	    return ((temp*temp)%1000000007);
+// 	}
+	
+	
+	//tabulaiton
 	int TotalWays(int N){
-	    // Code here
-	    vector<long long int>dp(N+1,-1);
-	    long long int temp=f(N,dp);
-	    return ((temp*temp)%1000000007);
+	    vector<long long int>dp(N+1,0);
+	    dp[1]=2;
+	    dp[2]=3;
+	    for(int i=3;i<=N;i++)
+	        dp[i]=(dp[i-1]+dp[i-2])%1000000007;
+	    return ((dp[N]*dp[N])%1000000007);
 	}
 };
 
