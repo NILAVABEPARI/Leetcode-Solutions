078712cfb38e3b373a7875644b7283c375aa0cb1
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-06-26 00:07:30 +0530

    Added solution - LeetHub

--- Fill the Tank - GFG/fill-the-tank.cpp
+++ Fill the Tank - GFG/fill-the-tank.cpp
@@ -7,31 +7,27 @@ using namespace std;
 
 class Solution {
 public:
-    using ll = long long ; 
-    vector<vector<int>> g ; 
-    ll dfs(int u, int p, int *cap, int n){
-       ll maxi = 0 ; 
-       ll mini = 1e18 ;
-       ll c = 0 ; 
-       for(int& v : g[u]){
-           if(v != p) {
-               ll x = dfs(v , u , cap , n) ; 
-               maxi = max(maxi , x) ; 
-               mini = min(mini , x) ;
-               c++ ; 
-           }
-       }
-       if(mini == -1) return mini ; 
-       if(cap[u - 1] > (ll)1e18 -  c * maxi) return -1 ;
-       return cap[u - 1] + c*maxi ;
+    long long dfs(vector<vector<int>>&g, int curr, int par, int *cap){
+        long long req=0,child=0;
+        for(auto &it:g[curr]){
+            if(it!=par){
+                child++;
+                long long chReq=dfs(g,it,curr,cap);
+                if(chReq==-1) return -1;
+                req=max(req,chReq);
+            }
+        }
+        if((child*req)>(long long)1e18-cap[curr-1]) return -1;
+        return child*req+cap[curr-1];
     }
-    long long minimum_amount(vector<vector<int>> &edges, int n, int src, int *cap){
-         g = vector<vector<int>>(n + 1) ;
-         for(auto e : edges){
-             g[e[0]].push_back(e[1]) ; 
-             g[e[1]].push_back(e[0]) ; 
-         }
-         return dfs(src , src , cap , n) ; 
+    long long minimum_amount(vector<vector<int>> &edges, int N, int s, int *cap){
+        // Code here
+        vector<vector<int>> g(N+1);
+        for(auto e : edges){
+            g[e[0]].push_back(e[1]);
+            g[e[1]].push_back(e[0]);
+        }
+        return dfs(g , s , -1, cap);
     }
 };
 
