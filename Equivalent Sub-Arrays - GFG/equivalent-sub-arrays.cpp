a7091adf664777b6f7ea11f218f86ae57ac6f4b9
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-06-26 00:23:31 +0530

    Added solution - LeetHub

---
+++ Equivalent Sub-Arrays - GFG/equivalent-sub-arrays.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+public:
+    int countDistinctSubarray(int nums[], int n){
+        //code here.
+        unordered_map<int,int>mp;
+        int i=0,cnt=0;
+        for(int i=0;i<n;i++)
+            mp[nums[i]]++;
+        int maxi=mp.size();
+        mp.clear();
+        for(int j=0;j<n;j++){
+            mp[nums[j]]++;
+            while(mp.size()>=maxi && i<=j){
+                cnt+=(n-j);
+                mp[nums[i]]--;
+                if(mp[nums[i]]==0) mp.erase(nums[i]);
+                i++;
+            }
+        }
+        return cnt;
+    }
+};
+
+
+// { Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+		Solution ob;	
+		cout<<ob.countDistinctSubarray(a,n)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
