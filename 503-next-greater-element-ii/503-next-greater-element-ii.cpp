c93557176d55f7a2cdacc384f842ca4e9b7ec7ca
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-06-25 16:06:07 +0530

    Time: 64 ms (30.68%), Space: 24 MB (42.23%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -7,9 +7,8 @@ public:
         st.push(nums[n-1]);
         ans[n-1]=-1;
         for(int i=2*n-1;i>=0;i--){
-            while(!st.empty() && nums[i%n]>=st.top()){
-               st.pop(); 
-            }
+            while(!st.empty() && nums[i%n]>=st.top()) st.pop(); 
+            
             if(st.empty()) ans[i%n]=-1;
             else ans[i%n]=st.top();
             st.push(nums[i%n]);
