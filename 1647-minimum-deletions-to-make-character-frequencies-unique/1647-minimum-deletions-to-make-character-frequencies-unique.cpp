18bd9376d9d845a7019d09741e96fedf2d508973
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-06-28 11:27:33 +0530

    Time: 88 ms (54.82%), Space: 17.3 MB (58.78%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -5,17 +5,16 @@ public:
         unordered_set<int>hasAppeared;
         int freq[26]={0};
         for(int i=0;i<n;i++) freq[s[i]-'a']++;
-        sort(freq,freq+26);
         for(int i=0;i<26;i++){
-            if(freq[i]!=0){
+            if(freq[i]){
                 if(hasAppeared.count(freq[i])==0)
                     hasAppeared.insert(freq[i]);
                 else{
                     int j=1;
                     while(hasAppeared.count(freq[i]-j)!=0) j++;
                     cnt+=j;
-                    freq[i]=freq[i]-j;
-                    if(freq[i]!=0) hasAppeared.insert(freq[i]);
+                    freq[i]-=j;
+                    if(freq[i]) hasAppeared.insert(freq[i]);
                 }
             }                
         }        
