d6af742309fbf425e12f7efe813c6d4c50634284
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-07-02 13:29:09 +0530

    Time: 4 ms (82.45%), Space: 12.7 MB (72.17%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,26 +12,28 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map < int, map < int, multiset < int >>> nodes; // vertical level values
-        queue < pair < TreeNode * , pair < int, int >>> todo;  // node vertical level
-        todo.push({root,{0,0}}); //root vertical and level
-        while (!todo.empty()) {
-            auto p = todo.front();
-            todo.pop();
-            TreeNode * temp = p.first;
-            //x -> vertical , y->level
-            int x = p.second.first, y = p.second.second;
-            nodes[x][y].insert(temp -> val); //inserting to multiset
-            if (temp -> left) 
-              todo.push({temp -> left,{x - 1,y + 1}});
-            if (temp -> right) 
-              todo.push({temp -> right,{x + 1,y + 1}});
+        map < int, map < int, multiset < int >>> mp; // vertical level values
+        queue < pair < TreeNode * , pair < int, int >>> q;  // node vertical level
+        q.push({root,{0,0}}); //root vertical and level
+        while (!q.empty()) {
+            auto p = q.front();  q.pop();
+            //p->{node, {vertical, level}}
+            TreeNode * node = p.first;
+            int vertical = p.second.first, level = p.second.second;
+            mp[vertical][level].insert(node -> val); //inserting to multiset
+            if (node -> left) 
+                q.push({node -> left,{vertical - 1,level + 1}});
+            if (node -> right) 
+                q.push({node -> right,{vertical + 1,level + 1}});
         }
         vector < vector < int >> ans;
-        for (auto p: nodes) {
-            vector < int > col;
-            for (auto q: p.second) 
-                col.insert(col.end(), q.second.begin(), q.second.end());
+        for (auto &p: mp) {
+            //p->{vertical, {level, values}}
+            vector < int > col; //store values in each vertical
+            for (auto &vertical: p.second) 
+                //vertical->{level, values}
+                //there can be multiple values on the same level
+                col.insert(col.end(), vertical.second.begin(), vertical.second.end());
             ans.push_back(col);
         }
         return ans;
