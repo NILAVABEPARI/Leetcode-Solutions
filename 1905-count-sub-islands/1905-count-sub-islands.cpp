7bacd321ca5a01ada86c74fceb28f6523eb07aa3
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-07-09 10:35:19 +0530

    Time: 558 ms (36.28%), Space: 88.4 MB (86.83%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,41 +1,66 @@
 class Solution {
 public:
-    void f(int i, int j, vector<vector<bool>>&vis, vector<vector<int>>&grid2,
-           vector<pair<int,int>>&ds){
-        if(i>=grid2.size() || j>=grid2[0].size() || i<0 || j<0 || 
-           vis[i][j] || grid2[i][j]==0) 
-            return;
-        vis[i][j]=true;
-        ds.push_back({i,j});
-        f(i+1,j,vis,grid2,ds);
-        f(i-1,j,vis,grid2,ds);
-        f(i,j+1,vis,grid2,ds);
-        f(i,j-1,vis,grid2,ds);
-    }
-    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        int cnt=0,n=grid2.size(),m=grid2[0].size();
-        vector<vector<bool>>vis(n,vector<bool>(m,false));
-        vector<vector<pair<int,int>>>arr;
+    // void f(int i, int j, vector<vector<bool>>&vis, vector<vector<int>>&grid2,
+    //        vector<pair<int,int>>&ds){
+    //     if(i>=grid2.size() || j>=grid2[0].size() || i<0 || j<0 || 
+    //        vis[i][j] || grid2[i][j]==0) 
+    //         return;
+    //     vis[i][j]=true;
+    //     ds.push_back({i,j});
+    //     f(i+1,j,vis,grid2,ds);
+    //     f(i-1,j,vis,grid2,ds);
+    //     f(i,j+1,vis,grid2,ds);
+    //     f(i,j-1,vis,grid2,ds);
+    // }
+    // int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+    //     int cnt=0,n=grid2.size(),m=grid2[0].size();
+    //     vector<vector<bool>>vis(n,vector<bool>(m,false));
+    //     vector<vector<pair<int,int>>>arr;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<m;j++){
+    //             if(!vis[i][j] && grid2[i][j]==1){
+    //                 vector<pair<int,int>>ds;
+    //                 f(i,j,vis,grid2,ds);
+    //                 arr.push_back(ds);  
+    //             }
+    //         }
+    //     }
+    //     for(auto &it:arr){
+    //         bool flag=true;
+    //         for(auto &i:it){
+    //             int x=i.first,y=i.second;
+    //             if(!grid1[x][y]){
+    //                 flag=false;
+    //                 break;
+    //             }
+    //         }
+    //         if(flag) cnt++;
+    //     }
+    //     return cnt;
+    // }
+    
+    
+    
+    //optimal
+    int f(int i, int j, vector<vector<int>>&grid1, vector<vector<int>>&grid2){
+        if(i>=grid2.size() || j>=grid2[0].size() || i<0 || j<0 || grid2[i][j]==0) 
+            return 1;
+        grid2[i][j]=0;
+        int res=1;
+        res&=f(i+1,j,grid1,grid2);
+        res&=f(i-1,j,grid1,grid2);
+        res&=f(i,j+1,grid1,grid2);
+        res&=f(i,j-1,grid1,grid2);
+        return res&grid1[i][j];
+    }    
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2){
+        int res=0,n=grid2.size(),m=grid2[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid2[i][j]==1){
-                    vector<pair<int,int>>ds;
-                    f(i,j,vis,grid2,ds);
-                    arr.push_back(ds);          
-                }
+                if(grid2[i][j]==1)
+                    res+=f(i,j,grid1,grid2);
             }
         }
-        for(auto &it:arr){
-            bool flag=true;
-            for(auto &i:it){
-                int x=i.first,y=i.second;
-                if(!grid1[x][y]){
-                    flag=false;
-                    break;
-                }
-            }
-            if(flag) cnt++;
-        }
-        return cnt;
+        return res;
     }
 };
