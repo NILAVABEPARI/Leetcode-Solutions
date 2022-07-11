475c220ffcd416c2bb1faa540a6788c19c9cb9c4
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-07-11 13:49:29 +0530

    Time: 32 ms (57.22%), Space: 13.3 MB (81.53%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,27 +1,59 @@
 class Solution {
 public:
-    bool dfsRec(vector<vector<int>>&graph, vector<bool>&vis,  vector<bool>&recSt, int s){
-        vis[s]=true;
-        recSt[s]=true;
-        for(int i:graph[s]){
-            if(vis[i]==false && dfsRec(graph,vis,recSt,i))
-                return true;
-            else if(recSt[i]==true)
-                return true;
-        }
-        recSt[s]=false;
-        return false;
-    }
+    //DFS algo
+    // bool dfsRec(vector<vector<int>>&graph, vector<bool>&vis,  vector<bool>&recSt, int s){
+    //     vis[s]=true;
+    //     recSt[s]=true;
+    //     for(int i:graph[s]){
+    //         if(vis[i]==false && dfsRec(graph,vis,recSt,i))
+    //             return true;
+    //         else if(recSt[i]==true)
+    //             return true;
+    //     }
+    //     recSt[s]=false;
+    //     return false;
+    // }
+    // bool canFinish(int numCourses, vector<vector<int>>& pre) {
+    //     vector<vector<int>>graph(numCourses);
+    //     int n=pre.size();
+    //     for(int i=0;i<n;i++)
+    //         graph[pre[i][1]].push_back(pre[i][0]);
+    //     vector<bool>vis(numCourses,false),recSt(numCourses,false);
+    //     for(int i=0;i<numCourses;i++){
+    //         if(!vis[i] && dfsRec(graph,vis,recSt,i))
+    //             return false;
+    //     }
+    //     return true;
+    // }
+    
+    
+    //BFS algo -- kahn's
     bool canFinish(int numCourses, vector<vector<int>>& pre) {
         vector<vector<int>>graph(numCourses);
         int n=pre.size();
         for(int i=0;i<n;i++)
             graph[pre[i][1]].push_back(pre[i][0]);
-        vector<bool>vis(numCourses,false),recSt(numCourses,false);
-        for(int i=0;i<numCourses;i++){
-            if(!vis[i] && dfsRec(graph,vis,recSt,i))
-                return false;
-        }
-        return true;
+        queue<int> q;
+	    vector<int> indegree(numCourses);
+	    for(int i = 0;i<numCourses;i++) {
+	        for(auto it: graph[i])
+	            indegree[it]++;
+	    }
+	    for(int i = 0;i<numCourses;i++) {
+	        if(indegree[i] == 0)
+	            q.push(i);
+	    }
+	    int cnt = 0;
+	    while(!q.empty()) {
+	        int node = q.front(); q.pop(); 
+	        cnt++; 
+	        for(auto it : graph[node]) {
+	            indegree[it]--;
+	            if(indegree[it] == 0)
+	                q.push(it); 
+	        }
+	    }
+	    if(cnt == numCourses) return true; 
+	    return false;
     }
 };
