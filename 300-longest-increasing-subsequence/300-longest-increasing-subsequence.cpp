a741b05950655e338f7ca8f24a9635bd97bc8280
NILAVA BEPARI <67020402+NILAVABEPARI@users.noreply.github.com>
2022-08-08 11:22:32 +0530

    Time: 4 ms (99.11%), Space: 10.5 MB (69.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -51,6 +51,37 @@ public:
     // }
     
     
+    //printing the LIS
+    // int lengthOfLIS(vector<int>& nums){
+    //     int n=nums.size();
+    //     vector<int>dp(n,1),hash(n);
+    //     int maxi=1,lastInd=0;
+    //     for(int i=0;i<n;i++){
+    //         hash[i]=i;
+    //         for(int prev=0;prev<i;prev++){
+    //             if(nums[prev]<nums[i] && 1+dp[prev]>dp[i]){
+    //                 dp[i]=1+dp[prev];
+    //                 hash[i]=prev;
+    //             }
+    //         }
+    //         if(dp[i]>maxi){
+    //             maxi=dp[i];
+    //             lastInd=i;
+    //         }
+    //     }        
+    //     vector<int>temp;
+    //     temp.push_back(nums[lastInd]);
+    //     while(hash[lastInd]!=lastInd){
+    //         lastInd=hash[lastInd];
+    //         temp.push_back(nums[lastInd]);        
+    //     }
+    //     reverse(temp.begin(),temp.end());
+    //     for(auto &it:temp) cout<<it<<" ";
+    //     cout<<endl;
+    //     return maxi;
+    // }
+    
+    
     //binary search
     int lengthOfLIS(vector<int>& nums){
         int len=1,n=nums.size();
